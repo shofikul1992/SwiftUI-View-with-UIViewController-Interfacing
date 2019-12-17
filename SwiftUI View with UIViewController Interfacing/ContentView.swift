76619eb5625f4718f32eefa02bc93925fce9c173
Shofikul Islam <shofikul1992@gmail.com>
2019-12-17 22:42:18 +0600

    Initial Commit

---
+++ SwiftUI View with UIViewController Interfacing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI View with UIViewController Interfacing
+//
+//  Created by Shofikul Islam on 17/12/2019.
+//  Copyright © 2019 Shofikul Islam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI View with UIViewController InterfacingTests/SwiftUI_View_with_UIViewController_InterfacingTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_View_with_UIViewController_InterfacingTests.swift
+//  SwiftUI View with UIViewController InterfacingTests
+//
+//  Created by Shofikul Islam on 17/12/2019.
+//  Copyright © 2019 Shofikul Islam. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_View_with_UIViewController_Interfacing
+
+class SwiftUI_View_with_UIViewController_InterfacingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
